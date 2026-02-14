389bee795df3a0fe1574e48ef051bef49e8bc833
akeriun <arunvikram515@gmail.com>
2026-02-14 18:56:42 +0530

    Update next.config.mjs
    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/Caches" : "",
-  assetPrefix: isProd ? "/Caches/" : "",
 };
 
 export default nextConfig;
